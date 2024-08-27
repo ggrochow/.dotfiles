88ccca5b9a020e1824b8d81769f32becedef4f76
Graham Grochowski <ggrochow@gmail.com>
2024-08-27 15:57:33 -0700

    Set leader+y to yank * buffer for better x11 system clip

--- config/nvim/lua/ggrochow/remap.lua
+++ config/nvim/lua/ggrochow/remap.lua
@@ -21,8 +21,8 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- copy to system clip
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["*y]])
+vim.keymap.set("n", "<leader>Y", [["*Y]])
 
 -- vertical edit mode fix
 vim.keymap.set("i", "<C-c>", "<Esc>")
